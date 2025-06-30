0d861e530b91ebe2dc49d097764706e058b5d465
ANANT-1510 <163185234+ANANT-1510@users.noreply.github.com>
2025-07-01 00:02:23 +0530

    Cretaed venv environment again beacause it crashed and then did data tranfromation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
   train_data_path:str=os.path.join('artifacts',"train.csv") ## artifacts is folder and train.csv is file
@@ -44,4 +46,7 @@ class DataIngestion:
 
 if __name__=="__main__":
   ob=DataIngestion()
-  ob.initiate_data_ingetsion()
+  train_data,test_data=ob.initiate_data_ingetsion()
+
+  data_transfromation=DataTransformation()
+  data_transfromation.initiate_data_tranformation(train_data,test_data)
