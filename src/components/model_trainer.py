d37c1630613d2834e29d358bd2d1064d38d494be
ANANT-1510 <163185234+ANANT-1510@users.noreply.github.com>
2025-07-01 19:46:01 +0530

    Performed hyperparamter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,11 +1,12 @@
 import os
 import sys
 from dataclasses import dataclass
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,LogisticRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 from sklearn.ensemble import AdaBoostRegressor,GradientBoostingRegressor,RandomForestRegressor
+from sklearn.svm import SVR
 from src.exception import CustomException
 from src.logger import logging
 
@@ -31,16 +32,56 @@ class ModelTrainer:
       )
 
       models={
-        "RandomForest":RandomForestRegressor(),
-        "LinearRegression":LinearRegression(),
-        "DecisionTreeRegression":DecisionTreeRegressor(),
+        "Decision Tree":DecisionTreeRegressor(),
+        "Random Forest":RandomForestRegressor(),
+        "Gradient Boosting":GradientBoostingRegressor(),
+        "Linear Regression":LinearRegression(),
+        "AdaBoost Regressor":AdaBoostRegressor(),
         "K-Neighbors":KNeighborsRegressor(),
-        "GradientBoosting":GradientBoostingRegressor(),
-        "AdaBoost Classifier":AdaBoostRegressor(),
-        "RandomForest Classifier":RandomForestRegressor()
+        "SVM":SVR()
       }
 
-      model_report:dict=evaluate_model(x_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,models=models)
+      params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    # 'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    # 'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['sqrt','log2'],
+                    # 'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    # 'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "SVM":{
+                  'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
+                  # 'degree':[1, 3, 5, 10],
+                  'gamma':['scale', 'auto']
+                },
+                "K-Neighbors": {
+                    'n_neighbors': [3, 5, 7, 9],
+                    # 'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                }
+                
+            }
+
+      model_report:dict=evaluate_model(x_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,models=models,params=params)
       
       ## To get the best model score from the dictionary
       best_model_score=max(sorted(model_report.values()))
